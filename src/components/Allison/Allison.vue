3a968b81d33df963b3d918a5d13c71392e0407fc
Allison Broughton <purplelady105@gmail.com>
2020-10-05 15:15:01 -0700

    Add appbar and page content. Still need to fix content so that appbar doesn't obscure it.

--- src/components/Allison/Allison.vue
+++ src/components/Allison/Allison.vue
@@ -1,29 +1,36 @@
 <template>
-	<v-container fluid fill-height>
+	<v-container fluid fill-height class="grey darken-4">
 		<v-row class="ma-0" align="center" justify="center">
-
+			<AppBar></AppBar>
+			<!-- <Banner></Banner> -->
+		</v-row>
+		<v-row class="ma-0" align="center" justify="center">
+			<v-card>
+				<v-card-title>Testing</v-card-title>
+				<v-card-text>
+					bla bla bla bla bla bla bla bla bla
+				</v-card-text>
+			</v-card>
 		</v-row>
 	</v-container>
 </template>
-
 <script>
+import AppBar from './components/appbar.vue';
+// import Banner from './components/banner.vue';
 export default {
 	name: "Allisons_Page",
 	components: {
+		AppBar,
+		// Banner
 	},
 	data() {
-		return {
-		}
+		return {}
 	},
-	methods: {
-	},
-	computed: {
-	},
-	watch: {
-	}
+	methods: {},
+	computed: {},
+	watch: {}
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
---
+++ src/components/Allison/components/appbar.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<v-app-bar
+		 absolute
+		 color="deep-purple"
+		 dark
+		 shrink-on-scroll
+		 src="https://picsum.photos/1920/1080?random">
+			<template v-slot:img="{ props }">
+				<v-img
+				 v-bind="props"
+				 gradient="to top right, rgba(124, 77, 255,.8), rgba(179, 136, 255,.6)"></v-img>
+			</template>
+			<!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+			<v-btn icon>
+				<v-icon>mdi-skip-backward-outline</v-icon>
+			</v-btn>
+			<v-toolbar-title>Allison Broughton</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn icon>
+				<v-icon>mdi-coffee</v-icon>
+			</v-btn>
+		</v-app-bar>
+	</div>
+</template>
