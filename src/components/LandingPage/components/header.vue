bcdf54c7bfb1557803a5740c3acdf2cac41cb737
X-vaylyne-O <brookeecobb@gmail.com>
2020-10-13 15:46:44 -0700

    added background, header, and subheader components

---
+++ src/components/LandingPage/components/header.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="headerPos headerText">
+		Hacktoberfest
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: "Header",
+	components: {
+	},
+	data() {
+		return {
+		}
+	},
+	methods: {
+	},
+	computed: {
+	},
+	watch: {
+	},
+	mounted() {
+
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.headerText {
+	/*font-family: 'Josefin Sans', sans-serif;*/
+	font-size: 8em;
+	line-height: 1em;
+	color: transparent;
+    -webkit-text-stroke-width: 2px;
+    -webkit-text-stroke-color: black;
+}
+</style>
