a6fd1a954e100e5a33f89a19b85695e4f17605eb
Kyle Henderson <kthenderson83@gmail.com>
2020-10-17 12:48:15 -0500

    add inner component

--- src/components/Kyle/index.vue
+++ src/components/Kyle/index.vue
@@ -14,7 +14,7 @@
 								:load-children="fetchUsers"
 								:open.sync="open"
 								activatable
-								color="warning"
+								color="primary"
 								open-on-click
 								transition
 							>
@@ -28,79 +28,7 @@
 
 						<v-divider vertical></v-divider>
 
-						<v-col class="d-flex text-center">
-							<v-scroll-y-transition mode="out-in">
-								<div
-									v-if="!selected"
-									class="title grey--text text--lighten-1 font-weight-light"
-									style="align-self: center"
-								>
-									Select a User
-								</div>
-								<v-card
-									v-else
-									:key="selected.id"
-									class="pt-6 mx-auto"
-									flat
-									max-width="400"
-								>
-									<v-card-text>
-										<v-avatar v-if="avatar" size="88">
-											<v-img
-												:src="`https://avataaars.io/${avatar}`"
-												class="mb-6"
-											></v-img>
-										</v-avatar>
-										<h3 class="headline mb-2">
-											{{ selected.name }}
-										</h3>
-										<div class="blue--text mb-2">
-											{{ selected.email }}
-										</div>
-										<div
-											class="blue--text subheading font-weight-bold"
-										>
-											{{ selected.username }}
-										</div>
-									</v-card-text>
-									<v-divider></v-divider>
-									<v-row class="text-left" tag="v-card-text">
-										<v-col
-											class="text-right mr-4 mb-2"
-											tag="strong"
-											cols="5"
-										>
-											Company:
-										</v-col>
-										<v-col>{{
-											selected.company.name
-										}}</v-col>
-										<v-col
-											class="text-right mr-4 mb-2"
-											tag="strong"
-											cols="5"
-										>
-											Website:
-										</v-col>
-										<v-col>
-											<a
-												:href="`//${selected.website}`"
-												target="_blank"
-												>{{ selected.website }}</a
-											>
-										</v-col>
-										<v-col
-											class="text-right mr-4 mb-2"
-											tag="strong"
-											cols="5"
-										>
-											Phone:
-										</v-col>
-										<v-col>{{ selected.phone }}</v-col>
-									</v-row>
-								</v-card>
-							</v-scroll-y-transition>
-						</v-col>
+						<SelectedItem v-if="selected" v-model="selected" />
 					</v-row>
 				</v-card>
 			</v-col>
@@ -109,19 +37,14 @@
 </template>
 
 <script>
-const avatars = [
-	"?accessoriesType=Blank&avatarStyle=Circle&clotheColor=PastelGreen&clotheType=ShirtScoopNeck&eyeType=Wink&eyebrowType=UnibrowNatural&facialHairColor=Black&facialHairType=MoustacheMagnum&hairColor=Platinum&mouthType=Concerned&skinColor=Tanned&topType=Turban",
-	"?accessoriesType=Sunglasses&avatarStyle=Circle&clotheColor=Gray02&clotheType=ShirtScoopNeck&eyeType=EyeRoll&eyebrowType=RaisedExcited&facialHairColor=Red&facialHairType=BeardMagestic&hairColor=Red&hatColor=White&mouthType=Twinkle&skinColor=DarkBrown&topType=LongHairBun",
-	"?accessoriesType=Prescription02&avatarStyle=Circle&clotheColor=Black&clotheType=ShirtVNeck&eyeType=Surprised&eyebrowType=Angry&facialHairColor=Blonde&facialHairType=Blank&hairColor=Blonde&hatColor=PastelOrange&mouthType=Smile&skinColor=Black&topType=LongHairNotTooLong",
-	"?accessoriesType=Round&avatarStyle=Circle&clotheColor=PastelOrange&clotheType=Overall&eyeType=Close&eyebrowType=AngryNatural&facialHairColor=Blonde&facialHairType=Blank&graphicType=Pizza&hairColor=Black&hatColor=PastelBlue&mouthType=Serious&skinColor=Light&topType=LongHairBigHair",
-	"?accessoriesType=Kurt&avatarStyle=Circle&clotheColor=Gray01&clotheType=BlazerShirt&eyeType=Surprised&eyebrowType=Default&facialHairColor=Red&facialHairType=Blank&graphicType=Selena&hairColor=Red&hatColor=Blue02&mouthType=Twinkle&skinColor=Pale&topType=LongHairCurly",
-];
+import Users from "./userData";
+import SelectedItem from "./selectedItem";
 
 export default {
 	name: "Kyles_Page",
+	components: { SelectedItem },
 	data: () => ({
 		active: [],
-		avatar: null,
 		open: [],
 		users: [],
 	}),
@@ -144,23 +67,19 @@ export default {
 		},
 	},
 
-	watch: {
-		selected: "randomAvatar",
-	},
+	watch: {},
 
 	methods: {
 		async fetchUsers(item) {
 			const pause = (ms) =>
 				new Promise((resolve) => setTimeout(resolve, ms));
-			await pause(1500);
-
-			return fetch("https://jsonplaceholder.typicode.com/users")
-				.then((res) => res.json())
-				.then((json) => item.children.push(...json))
-				.catch((err) => console.warn(err));
-		},
-		randomAvatar() {
-			this.avatar = avatars[Math.floor(Math.random() * avatars.length)];
+			try {
+				//
+				await pause(1500);
+				this.users = Users;
+			} catch (error) {
+				console.log(error);
+			}
 		},
 	},
 };
